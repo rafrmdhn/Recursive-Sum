59c84600635d8a76840671b85e21328ad27c72b5
Rafi Ramadhan <92287247+rafrmdhn@users.noreply.github.com>
2021-11-17 14:38:04 +0700

    Add files via upload

---
+++ RecursiveSum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int baris;
+    int i, j;
+
+    cin>>baris;
+
+    int n[baris];
+    int x[baris];
+    int y[baris];
+
+    for (i=0; i<baris; i++){
+        cin>>n[i]>>x[i]>>y[i];
+	}
+	
+	for(j=0; j<baris; j++){
+		for(int z=0; z<n[j]; z++){
+			if(z%x[j]==0 && z%y[j]!=0){
+            cout<<z<<" ";
+		}
+	}cout<<"\n";
+	
+}
+
+	return 0;
+}
